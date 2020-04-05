d37165ae4a98c6e8a7bb9fb629c593fc26414dde
Amol11 <amolsinghaniya@gmail.com>
2020-04-05 19:10:42 +0530

    custom events added

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'Amit',
+        text: 'yes it is.',
+        createdAt: 123123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log("Connection with a client lost.");
     });
