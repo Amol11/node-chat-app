be49ac8c55a3d3292874d76109c0ced1db5b0bde
Amol11 <amolsinghaniya@gmail.com>
2020-04-08 15:17:50 +0530

    formatted time stamps usimg moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@ var socket = io();
         });
 
         socket.on('newMessage', function (message){
-            console.log('newMessage', message);
+            var formattedTime = moment(message.createdAt).format('h:mm a');
             var li = jQuery('<li></li>');
-            li.text(`${message.from}: ${message.text}`);
+            li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
             jQuery('#messages').append(li);
         });
 
         socket.on('newLocationMessage', function (message){
+            var formattedLocationTime = moment(message.createdAt).format('h:mm a');
             var li = jQuery('<li>');
             var a = jQuery('<a target="_blank">My current location</a>');
 
-            li.text(`${message.from}: `);
+            li.text(`${message.from} ${formattedLocationTime}: `);
             a.attr('href', message.url);
             li.append(a);
             jQuery('#messages').append(li);
