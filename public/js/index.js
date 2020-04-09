13f05fcf145c0aa980b6aac19a6414085de982df
Amol11 <amolsinghaniya@gmail.com>
2020-04-09 13:38:24 +0530

    Added mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,37 @@ var socket = io();
 
         socket.on('newMessage', function (message){
             var formattedTime = moment(message.createdAt).format('h:mm a');
-            var li = jQuery('<li></li>');
-            li.text(`${message.from} ${formattedTime}: ${message.text}`);
+            var template = jQuery('#message-template').html();
+            var html = Mustache.render(template, {
+                text: message.text,
+                from: message.from,
+                createdAt: formattedTime
+            });
 
-            jQuery('#messages').append(li);
+            jQuery('#messages').append(html);
+            // var li = jQuery('<li></li>');
+            // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+            // jQuery('#messages').append(li);
         });
 
         socket.on('newLocationMessage', function (message){
             var formattedLocationTime = moment(message.createdAt).format('h:mm a');
-            var li = jQuery('<li>');
-            var a = jQuery('<a target="_blank">My current location</a>');
+            var template = jQuery('#location-message-template').html();
+            var html = Mustache.render(template, {
+                url: message.url,
+                from: message.from,
+                createdAt: formattedLocationTime
+            });
 
-            li.text(`${message.from} ${formattedLocationTime}: `);
-            a.attr('href', message.url);
-            li.append(a);
-            jQuery('#messages').append(li);
+            jQuery('#messages').append(html);
+            // var li = jQuery('<li>');
+            // var a = jQuery('<a target="_blank">My current location</a>');
+
+            // li.text(`${message.from} ${formattedLocationTime}: `);
+            // a.attr('href', message.url);
+            // li.append(a);
+            // jQuery('#messages').append(li);
         });
 
         jQuery('#message-form').on('submit', function (e){
