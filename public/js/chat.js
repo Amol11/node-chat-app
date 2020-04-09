15b453001490cde967f584f0927fbf2f00d76596
Amol11 <amolsinghaniya@gmail.com>
2020-04-09 14:54:11 +0530

    validation added at join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,7 +16,17 @@ function scrollToBottom () {
 }
 
         socket.on('connect', function () {
-            console.log("Connected to server");
+            var params = jQuery.deparam(window.location.search);
+
+            socket.emit('join', params, function (error) {
+                if(error){
+                    alert(error);
+                    window.location.href = '/';
+                }
+                else {
+                    console.log('No error');
+                }
+            });
         });
 
         socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message.js');
+const {isRealString} = require('./utils/validation.js');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -19,6 +20,14 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+    socket.on('join', (params, callback) => {
+        if(!isRealString(params.name) || !isRealString(params.room)){
+            callback('Name and room name are required.');
+        }
+
+        callback();
+    });
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
