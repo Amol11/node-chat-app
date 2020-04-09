8fe3c686aa869a050448be0922cc56cbb45bf322
Amol11 <amolsinghaniya@gmail.com>
2020-04-09 14:15:26 +0530

    added auto scrolling feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom () {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+        messages.scrollTop(scrollHeight);
+}
+
         socket.on('connect', function () {
             console.log("Connected to server");
         });
@@ -18,6 +33,7 @@ var socket = io();
             });
 
             jQuery('#messages').append(html);
+            scrollToBottom();
             // var li = jQuery('<li></li>');
             // li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
@@ -34,6 +50,7 @@ var socket = io();
             });
 
             jQuery('#messages').append(html);
+            scrollToBottom();
             // var li = jQuery('<li>');
             // var a = jQuery('<a target="_blank">My current location</a>');
 
